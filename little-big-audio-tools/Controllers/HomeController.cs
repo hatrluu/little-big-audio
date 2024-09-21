3866892025643dd508be377b98975b5443464478
Hau Luu <hatrluu@gmail.com>
2024-09-20 20:09:37 -0400

    Project init

---
+++ little-big-audio-tools/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using little_big_audio_tools.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace little_big_audio_tools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return File("~/index.html","text/html");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ little-big-audio-tools/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace little_big_audio_tools.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
